895c47cdfb232f8e286a5e7c01fd1f8f13c331ba
Adrian SanPedro <adrian.r.sanpedro@gmail.com>
2020-12-02 16:18:48 -0800

    Dev: Resize button functionality, markup, and style changes to resize grid by a number value entered through window prompt for a max size of 500px

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
+let squaresPerSide = 16;
+
 const mainGridContainerElement = document.getElementById('main-grid');
 const clearGridButtonElement = document.getElementById('clear-button');
+const resizeGridButtonElement = document.getElementById('resize-button');
 
 const squareClass = 'square';
 const squareHoveredClass = 'square-hovered';
 
+const GRID_SIZE = 500;
+
 //create a 16x16 grid of square divs
 function startGrid() {
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < squaresPerSide; i++) {
         const newRow = getRow();
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < squaresPerSide; j++) {
             const newSquare = getSquare();
             newRow.appendChild(newSquare);
         }
@@ -25,7 +30,11 @@ function getRow() {
 
 function getSquare() {
     const newSquare = document.createElement('div');
+    const squareSize = GRID_SIZE / squaresPerSide;
+
     newSquare.classList.add(squareClass);
+    newSquare.style.height = squareSize + 'px';
+    newSquare.style.width = squareSize + 'px';
     newSquare.addEventListener('mouseenter', function (e) {
         setHoverClass(e);
     });
@@ -48,6 +57,16 @@ function clearGrid() {
     });
 }
 
+function resizeGrid() {
+    squaresPerSide = Number(
+        window.prompt('Enter a number to be the new grid size.')
+    );
+    mainGridContainerElement.innerHTML = '';
+    clearGrid();
+    startGrid();
+}
+
 clearGridButtonElement.addEventListener('click', clearGrid);
+resizeGridButtonElement.addEventListener('click', resizeGrid);
 
 startGrid();
